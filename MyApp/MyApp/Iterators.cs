1ae2b90500b60a54d0600065fbbe2994649d7a7d
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-09 14:06:07 +0200

    Implemented Flatten() and Filter()

--- MyApp/MyApp/Iterators.cs
+++ MyApp/MyApp/Iterators.cs
@@ -2,7 +2,32 @@ namespace MyApp;
 
 public static class Iterators
 {
-    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items) => throw new NotImplementedException();
+    public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> items)
+    {
+        foreach(IEnumerable<T> i in items)
+        {
+            foreach(var v in i)
+            {
+                yield return v;
+            }
+        }
+        
+        
+    }
 
-    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate) => throw new NotImplementedException();
+    public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate)
+    {
+        predicate = Predicate
+        foreach(IEnumerable<T> i in items)
+        {
+            if(predicate){
+                yield return i;
+            }
+        }
+    }
+    public delegate bool Predicate<in T>(T obj);
+
+
+
+    
 }
