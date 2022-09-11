9ffdc3faf0fcfd899991f29871c4c22361c90090
Adam Hadou Temsamani <ahad@itu.dk>
2022-09-11 13:40:27 +0200

    Update IteratorsTests.cs

--- MyApp/MyApp.Tests/IteratorsTests.cs
+++ MyApp/MyApp.Tests/IteratorsTests.cs
@@ -2,4 +2,32 @@ namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Check_if_returns_stream_of_objects()
+    {
+        // Arrange
+            
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
+    [Fact]
+    public void Check_if_returns_only_even_numbers()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+
+    }
+
 }
